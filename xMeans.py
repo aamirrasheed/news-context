2724e3fef0799debaecb4b230eb96e53893c26a2
“duniahakim” <doniahakim@gmail.com>
2018-12-01 23:59:06 -0800

    2

--- xMeans.py
+++ xMeans.py
@@ -1,64 +1,40 @@
-import csv
-import random
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from nltk.tokenize import word_tokenize
+'''
+This file clusters our document embeddings using the xmeans method from the pyclustering package and 
+a trained model called "dv.model". It prints the clustered articles to the console.
+'''
+from gensim.models.doc2vec import Doc2Vec
 from pyclustering.cluster.xmeans import xmeans, splitting_type
+from main import getDataSets
 
-trainingDataSize = 1100
-testDataSize = 100
-
-def getarticlecontent(input_file):
-    data = []
-    for idx, row in enumerate(input_file):
-        data.append(row["Title"])
-    return data
-
-def getModelVectors(model):
+# this gets document embeddings based on article content
+def getModelVectors(model, trainingDataSize):
     vecs = [];
-    for i in range(0, trainingDataSize):
+    for i in range(trainingDataSize):
+        # model.docvecs[str(i)] gets the ith document embedding in our dataset
         vecs.append(model.docvecs[str(i)])
     return vecs
 
-def getLabeledData():
-    input_file = csv.DictReader(open("Stories.csv"))
-    data = getarticlecontent(input_file)
-    random.seed(1)
-    random.shuffle(data)
-    trainingData = data[0:trainingDataSize]
-    testData = data[trainingDataSize::]
 
-    tagged_training_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(trainingData)]
+def main():
+    # initialize variables
+    trainingData, trainingContent, testData, testContent = getDataSets()
+    model = Doc2Vec.load("dv.model")
+    vectors = getModelVectors(model, len(trainingData))
 
-    max_epochs = 10
-    vec_size = 20
-    alpha = 0.025
-
-    model = Doc2Vec(vector_size=vec_size,
-            alpha=alpha,
-            min_alpha=0.00025,
-            min_count=1,
-            dm=1)
-
-    model.build_vocab(tagged_training_data)
-
-    for epoch in range(max_epochs):
-        print('iteration {0}'.format(epoch))
-        model.train(tagged_training_data,
-                total_examples=model.corpus_count,
-                epochs=model.iter)
-        model.alpha -= 0.0002
-        model.min_alpha = model.alpha
-
-    vectors = getModelVectors(model)
+    # run xmeans and get clusters
     xmeans_instance = xmeans(vectors)
     xmeans_instance.process()
     clusters = xmeans_instance.get_clusters()
     numOfClusters = len(clusters)
 
-    trainingLabeled = [0 for x in range(trainingDataSize)]
+    # print out all clusters 
+    trainingLabeled = [0 for _ in range(len(trainingData))]
     for i in range(numOfClusters):
+        print("Cluster " + str(i + 1) + " (" + str(len(clusters[i])) + " articles in this cluster): ")
         for j in range(len(clusters[i])):
             trainingLabeled[clusters[i][j]] = i
+            print(trainingData[clusters[i][j]][0])
+        print("")  # just to seperate clusters when printing
 
-
-    return numOfClusters, trainingData, trainingLabeled, testData
+if __name__== "__main__":
+    main()
