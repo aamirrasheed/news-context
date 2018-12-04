9463a6522f0be18f2eae10ddc9e22071369696b8
“duniahakim” <doniahakim@gmail.com>
2018-12-03 18:33:20 -0800

    cleaned up and organized code

--- pretrainedModel_xMeans.py
+++ pretrainedModel_xMeans.py
@@ -1,8 +1,47 @@
+'''
+This file clusters our document embeddings using the xmeans method from the pyclustering package and
+a trained model called "wikiModel.bin". It prints the clustered articles to the console and labels
+the training data according to those clusters.
+'''
 import csv
+import random
 from gensim.models.doc2vec import Doc2Vec
 from pyclustering.cluster.xmeans import xmeans, splitting_type
 from nltk.tokenize import word_tokenize
-from main import getDataSets
+
+# returns title and article contents from our stories.csv file
+def getDataFromFile(input_file):
+    data = []
+    for idx, row in enumerate(input_file):
+        data.append((row["Title"], row["Content no HTML"]))
+    return data
+
+# given a data output from getDataFromFile(), this method will extract the article content
+def getContent(data):
+    content = []
+    for pair in data:
+        content.append(pair[1])
+    return content
+
+# this method is responsible for retrieving the data and dividing it into train and test sets
+def getDataSets():
+    # specify size of train set
+    trainingDataSize = 1100
+
+    # get dataset
+    input_file = csv.DictReader(open("Stories.csv"))
+    data = getDataFromFile(input_file)
+
+    # shuffle data
+    random.seed(1)
+    random.shuffle(data)
+
+    # set variables for each set and return
+    trainingData = data[0:trainingDataSize]
+    trainingContent = getContent(trainingData)
+    testData = data[trainingDataSize::]
+    testContent = getContent(testData)
+    return trainingData, trainingContent, testData, testContent
 
 # this gets document embeddings based on article content
 def getModelVectors(model, trainingContent):
@@ -12,6 +51,16 @@ def getModelVectors(model, trainingContent):
         embeddings.append(model.infer_vector(tokenized_vec))
     return embeddings
 
+# this method prints out all clusters and returns the labeled training data
+def getLabelTrainingDatAndPrintClusters(numOfClusters, clusters):
+    trainingLabeled = [0 for _ in range(len(trainingData))]
+    for i in range(numOfClusters):
+        print("Cluster " + str(i + 1) + " (" + str(len(clusters[i])) + " articles in this cluster): ")
+        for j in range(len(clusters[i])):
+            trainingLabeled[clusters[i][j]] = i
+            print(trainingData[clusters[i][j]][0])
+        print("") #just to seperate clusters when printing
+    return trainingLabeled
 
 def main():
     # initialize variables
@@ -25,14 +74,7 @@ def main():
     clusters = xmeans_instance.get_clusters()
     numOfClusters = len(clusters)
 
-    # print out all clusters
-    trainingLabeled = [0 for _ in range(len(trainingData))]
-    for i in range(numOfClusters):
-        print("Cluster " + str(i + 1) + " (" + str(len(clusters[i])) + " articles in this cluster): ")
-        for j in range(len(clusters[i])):
-            trainingLabeled[clusters[i][j]] = i
-            print(trainingData[clusters[i][j]][0])
-        print("")  # just to seperate clusters when printing
+    trainingLabeled = getLabelTrainingDatAndPrintClusters(numOfClusters, clusters)
 
 
 if __name__== "__main__":
